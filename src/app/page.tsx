4bf0d527bc309f895ecd330a0bbbcfe6edaf8e31
Shawaal Nadeem <hafizshawalnadeem@gmail.com>
2023-08-12 04:41:31 -0700

    shadcn inherit & custom colors add & minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import AlertUI from "./components/alertDialog"
 
-
-export default function Home() {
+export default function LognSign() {
   return (
-    <>
-    
-    </>
+    <div>
+    <div className=" bg-black w-1/5 text-4xl max-xl:bg-grayLight max-xl:w-full max-lg:bg-grayDark max-md:bg-black-Home-BG ">Login</div>
+  <div className=' text-9xl'>Codenest@gmail.com</div>
+  <AlertUI/>
+
+    </div>
   )
 }
