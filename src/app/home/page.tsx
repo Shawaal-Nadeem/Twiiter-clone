5881eb1368ec91b944c030a5ae63185c15cffe2d
Shawaal Nadeem <hafizshawalnadeem@gmail.com>
2023-08-21 18:17:13 +0500

    Popups completed

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -8,6 +8,12 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover"
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+
 
 import { useContext } from "react";
 import { context } from "@/contextAPI/contextApi";
@@ -36,7 +42,7 @@ export default function Home() {
   });
 
 
-  const tweetData = (data: any) => {
+  const tweetData = (data: any, index:number) => {
     const [like, updateLike] = useState(false);
     let num = data.likesNumber;
     const [countLike, updateCountLike] = useState(num);
@@ -53,7 +59,7 @@ export default function Home() {
     const [show,setShow]=useState(false);
     // Tweet Data Return
     return (
-      <div
+      <div key={index}
         className={
           mode === false
             ? "flex flex-col w-[100%] border-[.5px] border-gray bg-mainBg "
@@ -92,19 +98,37 @@ export default function Home() {
       <PopoverTrigger asChild onClick={()=>setShow(false)}>
         <Image src={'/images/more.png'} alt="Loading...." width={18} height={18} className=" cursor-pointer"></Image>
       </PopoverTrigger>
-      {show===false?
-        <PopoverContent className="w-30 h-24">
-        <p onClick={()=>setShow(true)} className="text-sm text-muted-foreground cursor-pointer">
-          Edit tweet
-        </p>
-        <hr className=" w-full border border-solid border-[#CACACA] mt-2"/>
-        <p onClick={()=>setShow(true)} className="text-sm text-muted-foreground mt-2 cursor-pointer">
-          Delete
-        </p>
+     
+      <PopoverContent className="w-30 h-24">
+      <Dialog>
+      <DialogTrigger asChild onClick={()=>setShow(true)}>
+        <p className="text-sm text-muted-foreground cursor-pointer text-center">Edit tweet</p>
+        </DialogTrigger>
+        <hr className=" w-full border border-solid border-[#CACACA] mt-[10px]"/>
+        <DialogTrigger asChild onClick={()=>setShow(false)}>
+        <p className="text-sm text-muted-foreground cursor-pointer mt-[10px] text-center">Delete</p>
+      </DialogTrigger>
+      {show===true?<DialogContent className="sm:max-w-[375px]">
+        <div className=" flex items-center flex-col">
+      <h3 className=" text-black font-bold text-xl">Edit Tweet</h3>
+      <hr className=" w-full border border-[#CACACA] mt-3" />
+      <textarea className=" mt-8 border border-solid border-[#CACACA] focus:outline-none rounded-lg w-72 h-32 pt-3 pl-3 pr-3" placeholder="Type Tweet..." ></textarea>
+      <button className=" bg-black text-white w-[105px] h-8 rounded-lg mt-5">Update</button>
+      </div>
+      </DialogContent>
+      : <DialogContent className="sm:max-w-[425px]">
+       <div className=" flex items-center flex-col">
+      <div className=" flex items-center justify-center bg-[#F3DDDD] w-20 h-20 rounded-full"><Image src={'/images/trash.png'} alt="Loading..." width={40} height={40}></Image></div>
+      <h2 className=" text-black text-2xl font-bold text-center w-64 mt-3">Do you want to delete this tweet?</h2>
+      <div className=" mt-9 mb-3 flex gap-12">
+        <button className=" bg-[#CACACA] text-black w-[105px] h-8 rounded-lg">Cancel</button>
+        <button className=" bg-black text-white w-[105px] h-8 rounded-lg">Delete</button>
+      </div>
+      </div>
+      </DialogContent> }
+    </Dialog>
   </PopoverContent>
-        :
-        null
-        }
+       
     </Popover>
           </div>
         
