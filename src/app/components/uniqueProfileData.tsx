cae144db6f72c9372b4794b14b0d870de2d316f6
MaHSAan10 <125743964+MaHSAan10@users.noreply.github.com>
2023-09-11 01:26:26 -0700

    update

--- src/app/components/uniqueProfileData.tsx
+++ src/app/components/uniqueProfileData.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { useState } from "react";
+import { TweetData } from "./tweetData";
 import Image from "next/image";
 import tweets from "../utils/mock";
 import { useContext } from "react";
@@ -19,177 +20,83 @@ import {
 export const UniqueProfileData = ({ props }: { props: string }) => {
   const [like, updateLike] = useState(false);
 
-    const profileData = tweets.find((temp) => {
-        return temp.slug === props;
-    }) as any;
-  
+  const profileData = tweets.find((temp) => {
+    return temp.slug === props;
+  }) as any;
+
   const [name, setName] = useState(profileData.username);
   const [update, setUpdate] = useState(false);
   const nameUpdate = (newName: string) => {
-      setName(newName);
-      
-}
-  
-  const [show, setShow] = useState(false);
-    let dataTweet = tweets.filter((temp) => { return profileData.username === temp.username })
-  console.log(dataTweet);
-    const getContext = useContext(context);
-    const mode = getContext.mode;
+    setName(newName);
+  }
 
-    const TweetData = (data: any) => {
-      let num = data.likesNumber;
-        const [countLike, updateCountLike] = useState(num);
-        const toggleLike = () => {
-          if (like === false) {
-            updateLike(true);
-            if (countLike >= 0) updateCountLike(countLike + 1);
-          } else if (like === true) {
-            updateLike(false);
-            if (countLike > 0) updateCountLike(countLike - 1);
-          }
-        };
-        // Tweet Data Return
-        return (
-          <div
-            className={
-              mode === false
-                ? "flex flex-col w-[100%] border-[.5px] border-gray bg-mainBg "
-                : "flex flex-col w-[100%] border-[.5px] border-gray bg-black"
-            }
-          >
-            <div className=" flex justify-center w-full mt-7"> 
-              <div className=" flex justify-center w-[95%]"> <Image src={data.profile} alt="Loading..." width={180} height={180} className={profileData.slug==='my-profile'?" rounded-full ml-5 ":" rounded-full"}></Image></div>
-              {profileData.slug === 'my-profile' ? <>
-                  {/* PopOver Component ShadCN */}
-       <div className=" mt-5 mr-3">
-        <Popover>
-      <PopoverTrigger asChild onClick={()=>setShow(false)}>
-      <Image src={mode === false ? '/images/setting-2.png' : '/images/setting-2w.png'} alt="Loading..." width={20} height={20} className="cursor-pointer"></Image>
-      </PopoverTrigger>
-     
-      <PopoverContent className="w-30 h-24">
-      <Dialog>
-      <DialogTrigger asChild onClick={()=>setShow(true)}>
-        <p className="text-sm text-muted-foreground cursor-pointer text-center font-PoppinsMedium">Edit Profile</p>
-        </DialogTrigger>
-        <hr className=" w-full border border-solid border-[#CACACA] mt-[10px]"/>
-        <DialogTrigger asChild onClick={()=>setShow(false)}>
-        <p className="text-sm text-muted-foreground cursor-pointer mt-[10px] text-center font-PoppinsMedium">Copy Link</p>
-      </DialogTrigger>
-      {show===true?<DialogContent className="sm:max-w-[375px]">
-        <div className=" flex items-center flex-col">
-      <h3 className=" text-black font-bold text-xl font-SamsungSharpSansBold">Edit Profile</h3>
-      <hr className=" w-full border border-[#CACACA] mt-3" />
-      <p className=" mt-7 flex justify-self-start font-semibold text-black font-SamsungSharpSansBold">Change Username</p>
-      <input onChange={(e)=>{nameUpdate(e.target.value)}} className=" pl-3 border border-solid border-[#CACACA] focus:outline-none rounded-lg w-72 h-10 mt-5 font-PoppinsLight" placeholder={profileData.username} ></input>
-      <button className=" bg-black text-white w-[105px] h-8 rounded-lg mt-5 font-SamsungSharpSansBold" onClick={(() => { setShow(false) })}>Update</button>
+  const [show, setShow] = useState(false);
+  let dataTweet = tweets.filter((temp) => { return profileData.username === temp.username })
+  console.log(dataTweet);
+  const getContext = useContext(context);
+  const mode = getContext.mode;
+
+  const TweetDataProfile = (data: any) => {
+    // Tweet Data Return
+    return (
+      <div
+        className={
+          mode === false
+            ? "flex flex-col w-[100%] border-[.5px] border-gray bg-mainBg "
+            : "flex flex-col w-[100%] border-[.5px] border-gray bg-black"
+        }
+      >
+        <div className=" flex justify-center w-full mt-7">
+          <div className=" flex justify-center w-[95%]"> <Image src={data.profile} alt="Loading..." width={180} height={180} className={profileData.slug === 'my-profile' ? " rounded-full ml-5 " : " rounded-full"}></Image></div>
+          {profileData.slug === 'my-profile' ? <>
+            {/* PopOver Component ShadCN */}
+            <div className=" mt-5 mr-3">
+              <Popover>
+                <PopoverTrigger asChild onClick={() => setShow(false)}>
+                  <Image src={mode === false ? '/images/setting-2.png' : '/images/setting-2w.png'} alt="Loading..." width={20} height={20} className="cursor-pointer"></Image>
+                </PopoverTrigger>
+
+                <PopoverContent className="w-30 h-24">
+                  <Dialog>
+                    <DialogTrigger asChild onClick={() => setShow(true)}>
+                      <p className="text-sm text-muted-foreground cursor-pointer text-center font-PoppinsMedium">Edit Profile</p>
+                    </DialogTrigger>
+                    <hr className=" w-full border border-solid border-[#CACACA] mt-[10px]" />
+                    <DialogTrigger asChild onClick={() => setShow(false)}>
+                      <p className="text-sm text-muted-foreground cursor-pointer mt-[10px] text-center font-PoppinsMedium">Copy Link</p>
+                    </DialogTrigger>
+                    {show === true ? <DialogContent className="sm:max-w-[375px]">
+                      <div className=" flex items-center flex-col">
+                        <h3 className=" text-black font-bold text-xl font-SamsungSharpSansBold">Edit Profile</h3>
+                        <hr className=" w-full border border-[#CACACA] mt-3" />
+                        <p className=" mt-7 flex justify-self-start font-semibold text-black font-SamsungSharpSansBold">Change Username</p>
+                        <input onChange={(e) => { nameUpdate(e.target.value) }} className=" pl-3 border border-solid border-[#CACACA] focus:outline-none rounded-lg w-72 h-10 mt-5 font-PoppinsLight" placeholder={name} ></input>
+                        <button className=" bg-black text-white w-[105px] h-8 rounded-lg mt-5 font-SamsungSharpSansBold" onClick={(() => { setShow(false) })}>Update</button>
+                      </div>
+                    </DialogContent>
+                      : null}
+                  </Dialog>
+                </PopoverContent>
+
+              </Popover>
+            </div>
+          </> : null}
+        </div>
+
+        <div className=" flex flex-col items-center mt-5 mb-12">
+          <p className={mode === false ? " font-bold text-lg text-[black] font-SamsungSharpSansBold" : " font-bold text-lg text-[white] font-SamsungSharpSansBold"}>{name}</p>
+          <p className=" font-semibold text-[#787878] font-SamsungSharpSans text-sm">{profileData.email}</p>
+        </div>
+        {profileData.slug === 'my-profile' ? <h4 className={mode === false ? " text-[black] text-2xl ml-5 font-SamsungSharpSansBold mb-2" : " font-SamsungSharpSansBold text-[white] text-2xl ml-5 mb-2"}>My Posts</h4> : <h4 className={mode === false ? " font-SamsungSharpSansBold text-[black] text-2xl ml-5" : " font-SamsungSharpSansBold text-[white] text-2xl ml-5"}>Posts</h4>}
+
+        {dataTweet.map(TweetData)}
       </div>
-      </DialogContent>
-      : null }
-    </Dialog>
-  </PopoverContent>
-       
-    </Popover>
-          </div>
-                </> : null}
-             </div>
-            
-            <div className=" flex flex-col items-center mt-5 mb-12">
-              <p className={mode===false?" font-bold text-lg text-[black] font-SamsungSharpSansBold":" font-bold text-lg text-[white] font-SamsungSharpSansBold"}>{name}</p>
-              <p className=" font-semibold text-[#787878] font-SamsungSharpSans text-sm">{profileData.email}</p>
-            </div>
-            {profileData.slug==='my-profile'?<h4 className={mode === false ? " text-[black] text-2xl ml-5 font-SamsungSharpSansBold" : " font-SamsungSharpSansBold text-[white] text-2xl ml-5"}>My Posts</h4>:<h4 className={mode === false ? " font-SamsungSharpSansBold text-[black] text-2xl ml-5" : " font-SamsungSharpSansBold text-[white] text-2xl ml-5"}>Posts</h4>}
-            
-            {dataTweet.map(function display(temp: any, index: number) {
-              return (
-                <div key={index} className=" flex border-t-[1px] border-t-[#CACACA] w-full mt-4">
-                  <div className="flex flex-col w-[95%]">
-             <div className=" ml-5 mt-5 mb-5 flex gap-1 items-center">
-             <Image src={temp.profile} alt="Loading..." width={30} height={30} className=" rounded-full"></Image>
-            <p  className={
-                  mode === false
-                    ? "ml-[6px] mt-[2px] text-[12px] text-s font-[700] leading-[normal] tracking-[-0.048px] font-SamsungSharpSansBold"
-                    : "ml-[6px] mt-[2px] text-white text-[12px] text-s font-[700] leading-[normal] tracking-[-0.048px] font-SamsungSharpSansBold"
-                }>{name}</p>
-            <p
-                className="ml-[6px] mt-[3px]  text-grayLight text-[10px] font-[500] 
-              leading-[normal] tracking-[-0.04px] font-PoppinsMedium"
-              >
-                {temp.time}
-                {temp.unit} ago
-                  </p>
-                  </div>
-                  <p  className={
-                mode === false
-                  ? "ml-[60px] mt-[1px] w-[280px] text-[12px] font-[400] leading-[normal] tracking-[-0.04px] self-start font-PoppinsLight"
-                  : "ml-[60px] mt-[1px] text-[#fff] w-[280px] text-[12px] font-[400] leading-[normal] tracking-[-0.04px] self-start font-PoppinsLight"
-              }>{temp.content}</p>
-              {data.contentImage === null ? (
-              <div className="hidden"></div>
-            ) : (
-              <div>
-                <img
-                  src={temp.contentImage}
-                  alt="Loading..."
-                  className="ml-[58px] mt-[17px] w-[240px] h-[240px] rounded-[13px] flex-shrink-0"
-                />
-              </div>
-                  )}
-                     <div className="flex items-center w-[100%] mt-[25px]  mb-[22px] ">
-              {mode === false ? (
-                <Image
-                  onClick={() => {
-                    toggleLike();
-                  }}
-                  src={
-                    like === false
-                      ? "/images/heartWhite.png"
-                      : "/images/heartRed.png"
-                  }
-                  alt=""
-                  width={16}
-                  height={16}
-                  className="ml-[17px] cursor-pointer "
-                ></Image>
-              ) : (
-                <Image
-                  onClick={() => {
-                    toggleLike();
-                  }}
-                  src={
-                    like === false
-                      ? "/images/heartWhite.png"
-                      : "/images/heartRed.png"
-                  }
-                  alt=""
-                  width={16}
-                  height={16}
-                  className="ml-[17px] cursor-pointer "
-                ></Image>
-              )}
-              <p
-                className="ml-[5px] text-grayLight text-[10px] font-[500] 
-              leading-[normal] tracking-[-0.04px] font-PoppinsMedium"
-              >
-                {countLike} Like
-              </p>
-              
-              <CommentsPopup commentNum={temp.commentsNumber}/>
-           
-                  </div>
-                  </div>
-             <MorePopup/>
-            </div>
-              )
-            })}
-    
-            </div>
-        );
+    );
   };
   // Main Function Return
-    return (
-        <>
-         {TweetData(profileData)}
-        </>
-    )
+  return (
+    <>
+      {TweetDataProfile(profileData)}
+    </>
+  )
 }
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,22 +1,14 @@
-"use client";
 import tweets from "../utils/mock";
-import { useState } from "react";
 import Image from "next/image";
-import Link from "next/link";
-import { useContext } from "react";
-import { context } from "@/contextAPI/contextApi";
-import { MorePopup } from "../components/morePopup";
-import { CommentsPopup } from "../components/commentsPopup";
 import { LogoutButton } from "../components/logoutButton";
 import { ThemeSwitcher } from "../components/themeSwitcher";
 import { MyProfileBox } from "../components/myProfileBox";
 import { AddIconMobile } from "../components/addIconMobile";
 import { RightSideHome } from "../components/rightSideHome";
+import { TweetData } from "../components/tweetData";
+import { TweetDataCall } from "../components/tweetData";
 
 export default function Home() {
-  const getContext = useContext(context);
-  const mode = getContext.mode;
-
   const tweetContent = tweets.filter((item: any) => {
     return item;
   });
@@ -25,105 +17,40 @@ export default function Home() {
     else return null;
   });
 
-// Problem part
-  const TweetData = (data: any, index:number) => {
-    const [like, updateLike] = useState(false);
-    let num = data.likesNumber;
-    const [countLike, updateCountLike] = useState(num);
-    const toggleLike = () => {
-      if (like === false) {
-        updateLike(true);
-        if (countLike >= 0) updateCountLike(countLike + 1);
-      } else if (like === true) {
-        updateLike(false);
-        if (countLike > 0) updateCountLike(countLike - 1);
-      }
-    };
-
-    // Tweet Data Return
-    return (
-      <div key={index} className={mode === false ? "flex flex-col w-[100%] border-[.5px] border-gray bg-mainBg " : "flex flex-col w-[100%] border-[.5px] border-gray bg-black"}>
-        <div className=" flex justify-between">
-          <div className="flex w-[80%] mt-[7px]">
-            <div>
-          <Link href={`/profiles/${data.slug}`}>
-            <img className="ml-[17px] mt-[15px] w-[32px] h-[32px] rounded-[32px] flex-shrink-0" src={data.profile} alt=""/>
-              </Link>
-              </div>
-            <div className=" flex items-center">
-              <Link href={`/profiles/${data?.slug}`}>
-            <h1 className={ mode === false ? "ml-[6px] mt-[2px] text-[12px] text-s font-[700] leading-[normal] tracking-[-0.048px] font-SamsungSharpSansBold" : "ml-[6px] mt-[2px] text-white text-[12px] text-s font-[700] leading-[normal] tracking-[-0.048px] font-SamsungSharpSansBold"}>
-            {data.username}
-                </h1>
-              </Link>
-          <p className="ml-[6px] mt-[2px]  text-grayLight text-[10px] font-[500] leading-[normal] tracking-[-0.04px] font-PoppinsMedium">
-            {data.time} {data.unit} ago
-              </p>
-              </div>
-        </div>
-        <MorePopup/>
-        
-        </div>
-        <div className={mode === false ? "ml-[55px] mt-[1px] w-[280px] text-[12px] font-PoppinsLight font-[500] text-black leading-[normal] tracking-[-0.04px] self-start" : "ml-[55px] mt-[1px] text-[#fff] w-[280px] text-[12px] font-PoppinsLight font-[500] leading-[normal] tracking-[-0.04px] self-start"}>
-          {data.content}
-        </div>
-        {data.contentImage === null ? (
-          <div className="hidden"></div>
-        ) : (
-          <div>
-            <img src={data.contentImage} alt="Loading..." className="ml-[58px] mt-[17px] w-[240px] h-[240px] rounded-[13px] flex-shrink-0"/>
-          </div>
-        )}
-        <div className="flex items-center w-[100%] mt-[25px]  mb-[22px] ">
-          {mode === false ? (
-            <Image onClick={() => {toggleLike();}} src={like === false ? "/images/heartWhite.png" : "/images/heartRed.png"} alt="" width={16} height={16} className="ml-[17px] cursor-pointer "></Image>
-          ) : (
-            <Image onClick={() => {toggleLike();}} src={like === false ? "/images/heartWhite.png" : "/images/heartRed.png"} alt="" width={16} height={16} className="ml-[17px] cursor-pointer "></Image>
-          )}
-          <p className="ml-[5px] text-grayLight text-[10px] font-[500] leading-[normal] tracking-[-0.04px] font-PoppinsMedium">
-            {countLike} Likes
-          </p>
-          <CommentsPopup commentNum={data.commentsNumber} />
-        </div>
-      </div>
-    );
-  };
-
   // Main Function Return
   return (
     <div
       className=" w-full bg-clip-content flex flex-col lmd:flex lmd:flex-row bg-white  dark:bg-black">
       <div className=" w-[100%] py-6 flex border-[.5px] mb-2 border-b-gray bg-mainBg lmd:hidden dark:bg-black">
         <div className="w-[90%] m-auto flex items-center justify-between">
-          <img src={myProfileData?.profile} alt="" className="w-[44px] h-[44px] rounded-[36px]"/>
+          <img src={myProfileData?.profile} alt="" className="w-[44px] h-[44px] rounded-[36px]" />
           <Image src={"/images/X.png"} alt="" width={55} height={55}></Image>
           <div className=" cursor-pointer rounded-[36px] ">
-            <ThemeSwitcher/>
+            <ThemeSwitcher />
           </div>
         </div>
       </div>
 
       {/* Left Side */}
       <div className="w-[25vw] hidden lmd:block h-[100vh] relative">
-     
-       <div className= "w-[25vw] h-[100vh] flex flex-col items-center  fixed dark:bg-black">
+
+        <div className="w-[25vw] h-[100vh] flex flex-col items-center  fixed dark:bg-black">
           <MyProfileBox myProfileData={myProfileData} />
-          <LogoutButton/>
+          <LogoutButton />
         </div>
       </div>
 
       {/* Mid Side */}
       <div className="w-[100%] m-auto relative overflow-x-hidden lmd:w-[50vw]  ">
-        <div className={ mode === false ? "w-[100%] hidden lmd:flex justify-center items-center border-l-[1px] border-r-[1px]  border-b-[1px] border-gray bg-mainBg" : "w-[100%] hidden lmd:flex justify-center items-center border-l-[1px] border-r-[1px] border-b-[1px] border-gray bg-black"}>
+        <div className="w-[100%] hidden lmd:flex justify-center items-center border-l-[1px] border-r-[1px]  border-b-[1px] border-gray bg-mainBg dark:bg-black">
           <Image src={"/images/X.png"} alt="" width={34} height={34} className="mt-[15px] mb-[17px]"></Image>
         </div>
-       {/* Problem Part */}
-        {tweetContent.map(TweetData)}
-        <AddIconMobile/>
+        <TweetDataCall/>
+        <AddIconMobile />
       </div>
 
-      <RightSideHome/>
-    
+      <RightSideHome />
+
     </div>
   );
 }
